dc2a6f8ff58cf5638e9ae7046ab550ffdeab78e9
woscc123 <woscc123@naver.com>
2021-02-15 22:48:32 +0900

    Add Python File [practice2.py]

---
+++ practice2.py
@@ -0,0 +1,31 @@
+# 습작
+# 클래스 내가 만들어보기
+# 전사 미니언 : 공격 유닛, 전사 , 검을 휘두를수잇음
+name = 'melee minion' # 유닛의 이름
+hp = 477 # 유닛의 체력
+damage = 12 # 유닛의 공격력
+attack_speed =1.25 # 유닛의 공격속도
+armor = 0 # 유닛의 방어력
+magic_resistance = 0 # 유닛의 마법저항력
+movement_speed = 325 # 유닛의 이동속도
+experience = 60.45 # 유닛의 경험치
+gold = 21 # 유닛의 골드
+range = 110 # 유닛의 사정거리
+
+print("{0} 이 생성되었습니다.".format(name))
+print("능력치:체력{0} 공격력{1} 공격속도 {2} 방어력 {3} 마법저항력{4} 이동속도{5} 경험치{6} 골드{7} 사정거리{8}".format(hp,damage,attack_speed,armor,magic_resistance,movement_speed,
+experience,gold,range))
+
+# 마법사 미니언 : 공격 유닛, 마법사, 빔을쏨
+name = 'caster minion' # 유닛의 이름
+hp = 296 # 유닛의 체력
+damage = 24 # 유닛의 공격력
+attack_speed = 0.667 # 유닛의 공격속도
+armor = 0 # 유닛의 방어력
+magic_resistance = 0 # 유닛의 마법저항력
+movement_speed = 325 # 유닛의 이동속도
+experience = 29.76 # 유닛의 경험치
+gold = 14 # 유닛의 골드
+range = 550 # 유닛의 사정거리
+
+def 
