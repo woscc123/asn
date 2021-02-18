a20213397b6cdec884962aab1cf83749eb3bb9ae
woscc123 <woscc123@naver.com>
2021-02-18 20:43:31 +0900

    Add Python File [if.py]

---
+++ if.py
@@ -0,0 +1,16 @@
+A=int(input("eos price?")) # str과 int사이의 비교
+
+if A>5000:
+    print('sell')    
+elif A<5000:
+    print('buy')
+else :
+    print('no')   
+
+한지웅 = input('한지웅은 이쁜이인가요?')
+if 한지웅=='착한일함':
+    print('이쁜이맞아요')
+elif 한지웅=='형괴롭힘':
+    print('안이뻐요')  
+
+ 
