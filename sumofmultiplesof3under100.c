39c0e53008fb553164c68ecac02e9aa7b62fff2b
woscc123 <woscc123@naver.com>
2021-07-31 17:57:46 +0900

    Add C File[sumofmultiplesof3under100.c]

---
+++ sumofmultiplesof3under100.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+
+	int i;
+	int sum;
+	
+	for(i=1;i<=100;i++)
+	if (i%3==0)
+	sum+=i;
+	
+	printf("%d",sum);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
